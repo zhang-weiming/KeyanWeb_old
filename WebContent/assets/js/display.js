e9ffc0285dd056e466694b05a8d338b66ea6c1a7
zwm <1210500550@qq.com>
2017-10-15 16:37:33 +0800

    发现并修复bug：display.html页面““是页面“提交成功”提示语只会显示一次；限制5s内只能提交一次

--- WebContent/assets/js/display.js
+++ WebContent/assets/js/display.js
@@ -1,7 +1,10 @@
 // display.js
 $(document).ready(function()
 {
-	URL_CONFIRM_SESSION = "confirmsession";
+	var URL_CONFIRM_SESSION = "confirmsession";
+	var time = 0;
+	var currentTime = 0;
+	var diff = 0;
 	$("textarea.sents").val("宝马的油耗太高了。红色的很嗲，白色敞篷的也喜欢的~。奇瑞汽车下线了这是结果，而不是奇瑞委屈的到处说自己一腔热血工业报国无门。奥迪的吉普中看不中用~苦逼的赶路孩子只能窝在最后座的小小空间里~。新览胜的灯更是无力吐槽。分享图片三选一，我最喜欢这台红色的雪佛兰，外观大气，空间大，中控台超有fell，我不喜欢丰田卡罗拉，老爸你要相信我的眼光，雪佛兰科鲁慈保值率高啊。比亚迪回复玻璃本性，没有默德萨克是最大败笔。个人比较讨厌开别克的，因为经常发现别克大白天的开着车灯甚至远光，每次被晃发现又是个别克，心中一万只羊驼奔腾，最近在上海各种原因各种机会开了各种别克，才发现原来几乎都是特么自动开灯的，别克的设计师大概都是偏远山区出来的吧，默认不是自动会死啊。速腾内饰也不好看。。比亚迪核心业务前景遭质疑-华尔街日报姐早就说了，千万不要买他们家的车子.。byd的策略根本就是有问题，为了圈钱，不是为了造车……");
 	$("span#sign_out").hide();
 	$.post(URL_CONFIRM_SESSION, function(result)
@@ -35,88 +38,112 @@ $(document).ready(function()
 	});
 	$("#input_text_commit").click(function()
 	{
-		$("p.error_info").html("提交成功！正在处理，请稍等。");
-		var sInput = $("textarea.sents").val();
-		$.post("puservlet", {
-			sents: sInput
-		}, function(result)
-		{
-			if(result != "null") 
+		currentTime = new Date().getTime();
+		diff = currentTime - time;
+		if (diff > 5000) 
+		{ // 两次请求间隔大于5s，允许请求。
+			time = currentTime;
+			$("p.error_info").html("提交成功！正在处理，请稍等。");
+			$("p.error_info").show();
+			var sInput = $("textarea.sents").val();
+			$.post("puservlet", {
+				sents: sInput
+			}, function(result)
 			{
-				$("p.error_info").hide();
 				var parts = result.split("|");
-				var pos_neg_counts = parts[0].split(" "); // pos neg 分别统计出来的个数
-				var pos_strs_positions = parts[1]; // 输入文本中，所有 pos 类句子的下标
-				pos_color='#F4A460';
-				neg_color='#7FFFD4';
-				$("#DC-legend").css("display", "block");
-				$("div.label-img").removeClass("before-img");
-				$("tr").remove(".tr-display");
-				positions = $.trim(pos_strs_positions).split(" ");
-				sInput_parts = sInput.split(/[。！？]/);
-				a = 1;
-				for(i = 0; i < sInput_parts.length; i++) 
-				{
-					if($.trim(sInput_parts[i]) != "") 
+				if(parts[0] == "null") 
+				{ // 没有分析结果
+					if (parts.length > 1)
 					{
-						temp_str = "<tr class=\"tr-display\">" + 
-										"<td class=\"td-column-label\">" + 
-											(a) + 
-										"</td>" + 
-										"<td " + "id=\"td-column-content-" + i + "\" class=\"td-column-content\">" + 
-											sInput_parts[i] + 
-										"</td>" + 
-									"</tr>";
-						$(".table-display").append(temp_str);
-						$("td#td-column-content-" + i).css("background-color", "#BFEFFF");
-						a++;
+						$("p.error_info").html(parts[1]);
+					}
+					else
+					{
+						$("p.error_info").html("没有分析结果！");
 					}
 				}
-				for(i = 0; i < positions.length; i++) 
-				{
-					$("td#td-column-content-" + positions[i]).css("background-color", "#FFDEAD");
-				}
-				$("div.pu-img").removeClass("before-img");
-				$("div#main1").css("height", "400px");
-				showpie(document.getElementById('main1'), [
-						{value: parseInt(pos_neg_counts[0]),name: '负面', selected:false}, 
-						{value: parseInt(pos_neg_counts[1]),name: '正面'}
-					]
-				);
-				$.post("transeservlet", 
-				{
-					positions: pos_strs_positions,
-					sents: sInput
-				}, function(result)
-				{
-					var pairs = result.split("|");
-					if(pairs[0] == "ERROR") 
+				else
+				{ // 有分析结果
+					$("p.error_info").hide();
+					var pos_neg_counts = parts[0].split(" "); // pos neg 分别统计出来的个数
+					var pos_strs_positions = parts[1]; // 输入文本中，所有 pos 类句子的下标
+					pos_color='#F4A460';
+					neg_color='#7FFFD4';
+					$("#DC-legend").css("display", "block");
+					$("div.label-img").removeClass("before-img");
+					$("tr").remove(".tr-display");
+					positions = $.trim(pos_strs_positions).split(" ");
+					sInput_parts = sInput.split(/[。！？]/);
+					a = 1;
+					for(i = 0; i < sInput_parts.length; i++) 
 					{
-						$("div#main2").html(pairs[1]);
-					}
-					else 
-					{
-						var vertex_arr = new Array(2 * pairs.length);
-						var arc_arr = new Array(pairs.length);
-						temp_vertex_str = "";
-						temp_arc_str = "";
-						for(var i = 0; i < pairs.length; i++) 
+						if($.trim(sInput_parts[i]) != "") 
 						{
-							var temp_pairs = pairs[i].split(" ");
-							vertex_arr[2 * i] = {category: 0, name: temp_pairs[0], value: 20};
-							vertex_arr[2 * i + 1] = {category: 1, name: temp_pairs[1], value: 20};
-							arc_arr[i] = {source: temp_pairs[0], target: temp_pairs[1] , weight: 1};
+							temp_str = "<tr class=\"tr-display\">" + 
+											"<td class=\"td-column-label\">" + 
+												(a) + 
+											"</td>" + 
+											"<td " + "id=\"td-column-content-" + i + "\" class=\"td-column-content\">" + 
+												sInput_parts[i] + 
+											"</td>" + 
+										"</tr>";
+							$(".table-display").append(temp_str);
+							$("td#td-column-content-" + i).css("background-color", "#BFEFFF");
+							a++;
 						}
-						arc_arr[0].weight = 5;
-						$("div.transE-img").removeClass("before-img");
-						$("div#main2").css("height", "400px");
-						showforce(document.getElementById('main2'), vertex_arr, arc_arr);
 					}
-				});
-			} // if
-			else {
-				$("p.error_info").html("抱歉，没有分析结果！");
-			}
-		});
+					for(i = 0; i < positions.length; i++) 
+					{
+						$("td#td-column-content-" + positions[i]).css("background-color", "#FFDEAD");
+					}
+					$("div.pu-img").removeClass("before-img");
+					$("div#main1").css("height", "400px");
+					showpie(document.getElementById('main1'), [
+							{value: parseInt(pos_neg_counts[0]),name: '负面', selected:false}, 
+							{value: parseInt(pos_neg_counts[1]),name: '正面'}
+						]
+					);
+					$.post("transeservlet", 
+					{
+						positions: pos_strs_positions,
+						sents: sInput
+					}, function(result)
+					{
+						var pairs = result.split("|");
+						if(pairs[0] == "ERROR") 
+						{
+							$("div#main2").html(pairs[1]);
+						}
+						else 
+						{
+							var vertex_arr = new Array(2 * pairs.length);
+							var arc_arr = new Array(pairs.length);
+							temp_vertex_str = "";
+							temp_arc_str = "";
+							for(var i = 0; i < pairs.length; i++) 
+							{
+								var temp_pairs = pairs[i].split(" ");
+								vertex_arr[2 * i] = {category: 0, name: temp_pairs[0], value: 20};
+								vertex_arr[2 * i + 1] = {category: 1, name: temp_pairs[1], value: 20};
+								arc_arr[i] = {source: temp_pairs[0], target: temp_pairs[1] , weight: 1};
+							}
+							arc_arr[0].weight = 5;
+							$("div.transE-img").removeClass("before-img");
+							$("div#main2").css("height", "400px");
+							showforce(document.getElementById('main2'), vertex_arr, arc_arr);
+						}
+					});
+				} // if
+	//			else 
+	//			{ // 
+	//				$("p.error_info").html("抱歉，没有分析结果！");
+	//			}
+			});
+		}
+		else
+		{ // 间隔时间太短，不给予请求
+			$("p.error_info").html("请等待" + (diff / 1000) + "秒再提交！");
+			$("p.error_info").show();
+		}
 	});
 });
--- src/myservlet/PUServlet.java
+++ src/myservlet/PUServlet.java
@@ -1,6 +1,8 @@
 package myservlet;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,20 +13,46 @@ import myjavabean.pu.PU;
 @WebServlet("/puservlet")
 public class PUServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private long time;
+	private long currentTime;
+	private long diff;
+	private String sents;
+	private String classifyResult;
     public PUServlet() {
         super();
+        time = 0;
+        currentTime = 0;
+        diff = 0;
     }
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
 		PrintWriter out = response.getWriter();
-		String sents = request.getParameter("sents"); // 获取输入文本
-		String classifyResult = PU.svm_classify(sents); // 文本分句、去停用词并分类
-		if(classifyResult != null) {
-			out.print(classifyResult); // 返回处理结果。格式：正例个数 负例个数|[正例在句子数组中的索引值...，以一个空格间隔] （例如：5 3|0 2 3 5 7）
-		}
-		else {
-			out.print("null"); // 处理结果为空
+		try {
+			currentTime = new Date().getTime();
+			diff = currentTime - time;
+			if (diff > 5000) 
+			{ // 两次请求间隔大于5s，允许请求。
+				time = currentTime;
+				sents = request.getParameter("sents"); // 获取输入文本
+				classifyResult = PU.svm_classify(sents); // 文本分句、去停用词并分类
+				if(classifyResult != null) {
+					out.print(classifyResult); // 返回处理结果。格式：正例个数 负例个数|[正例在句子数组中的索引值...，以一个空格间隔] （例如：5 3|0 2 3 5 7）
+				}
+				else 
+				{
+					out.print("null"); // 处理结果为空
+				}
+			}
+			else 
+			{ // 两次请求间隔小于等于5s，不允许请求。
+				System.out.println("上次时间：" + time);
+				System.out.println("当前时间：" + currentTime);
+				out.println("null|请" + (diff / 1000) + "秒后再提交！");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			out.println("null");
 		}
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
